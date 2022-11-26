2982bf099aa17b1ef4ced0f0826fa656f0459e36
Liron-Wassa <khruiuuxv@gmail.com>
2022-11-26 16:41:42 +0200

    finish task

--- task.c
+++ task.c
@@ -3,6 +3,85 @@
 
 int main () {
 
+    // int sumDigits(int numbers) {
+    //     int sum = 0;
+
+    //     while (numbers > 0) {
+    //         sum += numbers % 10;
+    //         numbers /= 10;
+    //     };
+
+    //     return sum;
+    // };
+
+    // int validateId (int id) {
+    //     int sum = 0, sumOfDigit, digitIndex = 1, numberToMultiple = 1;
+    
+    //     while (id > 0) {
+    //         numberToMultiple = digitIndex % 2 == 0 ? 2 : 1;
+    //         sumOfDigit = (id % 10) * numberToMultiple;
+
+    //         if (sumOfDigit > 9) {
+    //             sumOfDigit = (sumOfDigit % 10) + (sumOfDigit / 10);
+    //         };
+
+    //         digitIndex++;
+    //         sum += sumOfDigit;
+    //         id /= 10;
+    //     };
+
+    //     return sum % 10 == 0;
+    // };
+
+    // void printId (int number, int zeroPrefix) {
+    //     int i, digit;
+    //     for (i = 0; i < zeroPrefix; i++)
+    //     {
+    //         printf("0");
+    //     };
+
+    //     while (number > 0)
+    //     {
+    //         digit = number % 10;
+    //         printf("%c", 48 + digit);
+    //         number /= 10;
+    //     };
+
+    //     printf("\n");
+    // };
+
+    // int revertNumber (int number) {
+    //     int digit, revertedNumber = 0, numberDigit = log10(number), powDigit = pow(10, numberDigit);
+
+    //     while (number > 0)
+    //     {
+    //         digit = number % 10;
+    //         revertedNumber += digit * powDigit;
+    //         powDigit /= 10;
+    //         number /= 10;
+    //     };
+
+    //     return revertedNumber;
+    // };
+
+    // int numberLength(int number) {
+    //     return (int)log10(number) + 1;
+    // };
+
+    // int checkID (int id) {
+    //     int revertedNumber, numberDigit = numberLength(id), numberZeroToPrint = 0;
+
+    //     if (numberDigit < 9)
+    //     {
+    //         numberZeroToPrint = 9 - numberDigit;
+    //     };
+
+    //     revertedNumber = revertNumber(id);
+    //     printId(revertedNumber, numberZeroToPrint);
+
+    //     return validateId(id);
+    // };
+    
     // int sumLongestArithmeticList(int number) {
     //     int i;
     //     int digit;
@@ -65,10 +144,6 @@ int main () {
     //     };
 
     //     return maxSum;
-    // };
-
-    // int numberLength(int number) {
-    //     return (int)log10(number) + 1;
     // };
     
     // int rotateNumber(int spins, int number) {
