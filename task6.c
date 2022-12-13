76e9c4ce34996e7b6dd1261787c364a7ca7c7122
Liron-Wassa <khruiuuxv@gmail.com>
2022-12-13 16:56:38 +0200

    refactor

--- task6.c
+++ task6.c
@@ -344,5 +344,43 @@ int main() {
 
     // ============================================================================= laboratory 6.12
 
+    // int convertBinaryToDecimal(int number) {
+    //     if (number <= 0) return 0;
+    //     return (number % 10) + 2 * convertBinaryToDecimal(number / 10);
+    // };
+
+    // printf("%d\n", convertBinaryToDecimal(110));
+
+    // ============================================================================= additional 6.1
+
+    // int convertDecimalToBinary(int number) {
+    //     if (number <= 0) return 0;
+    //     return (number % 2) + 10 * convertDecimalToBinary(number / 2);
+    // };
+
+    // printf("%d\n", convertDecimalToBinary(2));
+
+    // ============================================================================= additional 6.2
+
+    // int rec(int number, int divider) {
+    //     if (divider == 0 || number == 0) return 0;
+    //     else if (number < 0 && divider < 0 && divider < number) return 0;
+    //     else if (number > 0 && divider > 0 && divider > number) return 0;
+    //     return rec(number - (divider), divider) + 1;
+    // };
+
+    // int rem(int number, int divider) {
+    //     if (divider == 0 || number == 0) return number;
+    //     else if (number < 0 && divider < 0 && divider < number) return number;
+    //     else if (number > 0 && divider > 0 && divider > number) return number;
+    //     return rec(number - (divider), divider);
+    // };
+
+    // printf("%d\n", rem(10, 6));
+
+    // printf("%d\n", 11 % 3);
+
+    // ============================================================================= additional 6.3
+
     return 0;
 };
