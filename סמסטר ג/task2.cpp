da4ebaf68e2fe76f78326c841149b2b9ad377476
Liron-Wassa <khruiuuxv@gmail.com>
2023-07-15 13:36:05 +0300

    finish task 2

--- סמסטר ג/task2.cpp
+++ סמסטר ג/task2.cpp
@@ -1,54 +1,82 @@
 #include <iostream>
 
 using namespace std;
+#define DATES_SIZE 5
 
-struct Clock {
-    int h;
-    int m;
-    int s;
+struct MyDate {
+    int day;
+    int month;
+    int year;
 };
 
-void swap1(int *p1, int *p2);
-void swap2(int &p1, int &p2);
-Clock myTimeClone(const Clock &myTime);
+void swapPointer(int *p1, int *p2);
+void swapRef(int &p1, int &p2);
+int checkDate(int &day, int &month, int &year);
+MyDate *saveDates(unsigned numDates);
+int printDates(MyDate *dates, unsigned size);
 
-void a(Clock a);
 int main()
 {   
     int val1 = 5, val2 = 10;
 
-    swap1(&val1, &val2);
-    cout << "swap1 - val1: " << val1 << " swap1 - val2: " << val2 << endl;
+    swapPointer(&val1, &val2);
+    cout << "swap pointer - val1: " << val1 << " swap pointer - val2: " << val2 << endl;
 
-    swap2(val1, val2);
-    cout << "swap2 - val1: " << val1 << " swap2 - val2: " << val2 << endl;
+    swapRef(val1, val2);
+    cout << "swap ref - val1: " << val1 << " swap ref - val2: " << val2 << endl;
 
-    // ============================================================================= Q.1
+    MyDate *dates = saveDates(DATES_SIZE);
+    printDates(dates, DATES_SIZE);
 
-    Clock original = { 1,2,3 };
-    Clock copied = myTimeClone(original);
-    original.h = 5;
-    cout << "original: " << original.h << endl;
-    cout << "copied: " << copied.h << endl;
-
-    // ============================================================================= Q.2
+    delete [] dates;
 };
 
-
-void swap1(int *p1, int *p2) {
+void swapPointer(int *p1, int *p2) {
     int temp = *p1;
     *p1 = *p2;
     *p2 = temp;
 };
 
-void swap2(int &p1, int &p2) {
+void swapRef(int &p1, int &p2) {
     int temp = p1;
     p1 = p2;
     p2 = temp;
 };
 
-Clock myTimeClone(const Clock &myTime) {
-    Clock copiedTime;
-    copiedTime = myTime;
-    return copiedTime;
+int checkDate(int &day, int &month, int &year) {
+    if ((month >= 1 && month <= 12) && (day >= 1 && day <= 31)) return 0;
+    swapRef(day, month);
+    return 1;
+};
+
+MyDate *saveDates(unsigned numDates) {
+    int i;
+    MyDate date;
+    MyDate *dates = new MyDate[numDates];
+
+    for (i = 0; i < numDates; i++) {
+        cout << "enter day" << endl;
+        cin >> date.day;
+        cout << "enter month" << endl;
+        cin >> date.month;
+        cout << "enter year" << endl;
+        cin >> date.year;
+
+        dates[i] = date;
+        cout << "entered day: " << dates[i].day << endl;
+    };
+
+    return dates;
+};
+
+int printDates(MyDate *dates, unsigned size) {
+    int i, counter = 0;
+
+    for (i = 0; i < size; i++) {
+        if (checkDate(dates[i].day, dates[i].month, dates[i].year)) counter++;
+        cout << "date: " << dates[i].day << "/" << dates[i].month <<  "/" << dates[i].year << endl;
+    };
+
+    counter && cout << "found " << counter << " invalid dates!!!" << endl;
+    return counter;
 };
